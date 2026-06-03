5d79354ee098fca4490185da30a5c1cb55b88f1a
Kunalsk36 <kunalkavathekar@gmail.com>
2026-06-03 17:48:38 +0530

    learn about pyjokes module

---
+++ chapter1/pyjokes_module.py
@@ -0,0 +1,12 @@
+# pip install pyjokes
+# This module is used to get random jokes in Python. It provides a simple interface to retrieve jokes in various categories and languages. You can use it to add some humor to your Python applications or just for fun.
+
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)
+print("\n")
+joke = pyjokes.get_joke(language="en", category="all")
+print(joke)
+print("\n")
+joke = pyjokes.get_joke(language="en", category="neutral")
+print(joke)
