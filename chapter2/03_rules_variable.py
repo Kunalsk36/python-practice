0c641a3586711fb5a1ed15123604610240b97d0b
Kunalsk36 <kunalkavathekar@gmail.com>
2026-06-04 13:22:53 +0530

    rules for defining variable and operators

---
+++ chapter2/03_rules_variable.py
@@ -0,0 +1,5 @@
+# A variable name can contain alphabets, digits, and underscores. 
+# A variable must start with an alphabet or an underscore. 
+# For example, 'my_variable', '_myVariable', 'variable1' are valid variable names in Python.
+# A variable can't start with digits. For example, '1variable' is not a valid variable name because it starts with a digit. However, 'variable1' is a valid variable name because it starts with an alphabet and contains digits.
+# No white space is allowed in variable names. For example, 'my variable' is not a valid variable name because it contains a space. Instead, you can use an underscore to separate words, like 'my_variable'.  
---
+++ chapter2/04_operators.py
@@ -0,0 +1,37 @@
+# Arithmetic Operators:
+a = 10
+b = 5
+c = a + b # Addition
+d = a - b # Subtraction
+e = a * b # Multiplication
+f = a / b # Division
+g = a % b # Modulus
+
+# Assignment Operators:
+x = 10
+x += 5 # x = x + 5
+x -= 3 # x = x - 3
+x *= 2 # x = x * 2
+x /= 4 # x = x / 4
+x %= 3 # x = x % 3
+
+
+# Comparison Operators:
+print(a == b) # Equal to
+print(a != b) # Not equal to
+print(a > b) # Greater than
+print(a < b) # Less than
+print(a >= b) # Greater than or equal to
+print(a <= b) # Less than or equal to
+
+
+# Logical Operators:
+# and, or, not
+# and - returns True if both conditions are true
+# or - returns True if at least one condition is true
+# not - returns the opposite of the condition
+a = 5
+b = 10
+print(a > 3 and b< 14) # True and True -> True
+print(a > 3 or b < 8) # True or False -> True
+print(not(a > 3)) # not True -> False
