b170d741a9cbc5a54829299f49cca076cdfec122
Kunalsk36 <kunalkavathekar@gmail.com>
2026-06-04 11:50:27 +0530

    learn about variabls and datatype

---
+++ chapter2/01_variables.py
@@ -0,0 +1,20 @@
+# A variable is a name given to memory location in a program.
+# For example:
+
+a = 10 # Here, 'a' is a variable that holds the value 10. We can use this variable to perform operations or store different values throughout the program. Variables are essential for storing and manipulating data in programming.
+
+b = 24.24 # Here, 'b' is a variable that holds the value 24.24, which is a floating-point number. We can use this variable to perform mathematical operations or store different values as needed.
+
+c = "Hello Kunal!" # Here, 'c' is a variable that holds the string "Hello Kunal!". We can use this variable to display messages, concatenate with other strings, or perform various string operations in our program.
+
+d = True # Here, 'd' is a variable that holds the boolean value True. We can use this variable to represent a condition or state in our program, such as whether a certain condition is met or not.
+
+e = None # Here, 'e' is a variable that holds the value None, which represents the absence of a value or a null value. We can use this variable to indicate that a variable has no value assigned to it or to represent a default state in our program.
+
+
+# variables - container to store a value.
+
+# keyword - reserved word in Python that cannot be used as a variable name. For example, 'if', 'else', 'while', 'for', etc. are keywords in Python.
+
+# identifier - name used to identify a variable, function, class, module, or other object in Python. It must follow certain rules, such as starting with a letter or underscore, and can contain letters, digits, and underscores. For example, 'my_variable', '_myVariable', 'variable1' are valid identifiers in Python.
+
---
+++ chapter2/02_datatype.py
@@ -0,0 +1,12 @@
+# Python has following primary data types:
+# 1. Integer
+# 2. Floating point number
+# 3. String
+# 4. Boolean 
+# 5. None
+
+a = 1 # integer
+b = 2.5 # floating point number
+c = "Hello Kunal!" # string 
+d = True # boolean
+e = None # NoneType
