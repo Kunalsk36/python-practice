89edddd8d8f12550c5b488d26473d03971195c98
Kunalsk36 <kunalkavathekar@gmail.com>
2026-06-04 13:40:03 +0530

    input function

---
+++ chapter2/07_input.py
@@ -0,0 +1,13 @@
+# input is used to take input from the user. It takes string input from the user and returns it as a string.
+
+a = input("Enter your name: ")
+print("Hello " + a + "! Welcome to Python programming.")
+
+# int(input()) is used to take integer input from the user. It takes string input from the user, converts it to an integer and returns it as an integer.
+
+b = int(input("Enter your age: "))
+print("You are " + str(b) + " years old.")
+
+# float(input()) is used to take floating point number input from the user. It takes string input from the user, converts it to a float and returns it as a float.
+c = float(input("Enter your height in meters: "))
+print("Your height is " + str(c) + " meters.")
