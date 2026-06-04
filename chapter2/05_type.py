ea0ea5fd314e179609e00613bcaab00dd6beca52
Kunalsk36 <kunalkavathekar@gmail.com>
2026-06-04 13:31:08 +0530

    type and type conversion

---
+++ chapter2/05_type.py
@@ -0,0 +1,16 @@
+a = 5
+print(type(a)) # <class 'int'>
+
+b = 3.14
+print(type(b)) # <class 'float'>
+
+c = "Hello Kunal!"
+print(type(c)) # <class 'str'>
+c = "36"
+print(type(c)) # <class 'str'>
+
+d = True
+print(type(d)) # <class 'bool'>
+
+e = None
+print(type(e)) # <class 'NoneType'>
---
+++ chapter2/06_type_conversion.py
@@ -0,0 +1,11 @@
+a = 32
+b = int(a) # converting a to integer
+print(b) # Output: 32
+
+k = "3.14"
+k = float(k) # converting k to float
+print(k) # Output: 3.14
+
+c = 465
+d = str(c) # converting c to string
+print(d) # Output: "465"
